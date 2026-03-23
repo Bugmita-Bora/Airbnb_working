ceed66252cafaeb0d3a8b4d5c15e50f591626acb
Bugmita-Bora <borabugmita009@gmail.com>
2026-03-23 20:32:20 +0530

    added MVC structur

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -1,27 +1,15 @@
-// Core Module
-const path = require("path");
-
 // External Module
 const express = require("express");
 const hostRouter = express.Router();
 
 // Local Module
-const rootDir = require("../utils/pathUtil");
+const homesController = require("../controllers/homes");
 
-hostRouter.get("/add-home", (req, res, next) => {
-  res.render("addHome", { pageTitle: "Add Home to airbnb" });
-});
+hostRouter.get("/add-home", homesController.getAddHome);
 
-const registeredHomes = [];
-
-hostRouter.post("/add-home", (req, res, next) => {
-  console.log("Home Registration successful for:", req.body);
-  registeredHomes.push(req.body);
-  res.render("homeAdded", { pageTitle: "Home Added Successfully" });
-});
+hostRouter.post("/add-home", homesController.postAddHome);
 
 exports.hostRouter = hostRouter;
-exports.registeredHomes = registeredHomes;
 
 // Browser hits POST /add-home  ← URL is defined ✅
 //         ↓
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,20 +1,10 @@
-// Core Modules
-const path = require("path");
-
 // External Module
 const express = require("express");
 const userRouter = express.Router();
 
-// Local Module
-const { registeredHomes } = require("./hostRouter");
+const homesController = require("../controllers/homes");
 
-userRouter.get("/", (req, res, next) => {
-  console.log(registeredHomes);
-  res.render("home", {
-    registeredHomes: registeredHomes,
-    pageTitle: "airbnb Home",
-  });
-});
+userRouter.get("/", homesController.getHomes);
 
 module.exports = userRouter;
 // registered home ko v destrucutr krlia kyuki hostrouter se multile obj arhethe na to jisko host chahey wo hist lo jisko registeredhomes chaheye wp registered homes lo
